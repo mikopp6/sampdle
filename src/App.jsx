52b8f5bcd575cf9d40d3042eb26fe5a434a20230
Miko Palojärvi <miko.p777@gmail.com>
2026-04-30 20:26:32 +0300

    rewrite

--- src/App.jsx
+++ src/App.jsx
@@ -1,77 +1,65 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import YoutubeEmbed from "./YoutubeEmbed";
 import AutoComplete from "./Autocomplete";
 
 import winner from "./assets/win.png";
-import originals from "./assets/originals.json";
-import samples from "./assets/samples.json";
+import matches from "./assets/matches.json";
+
+// Game states: 
+// 0 = not started 
+// 1 = started 
+// 2 = ended
 
 const App = () => {
-  const [gameStart, setGameStart] = useState(false);
-  const [gameOver, setGameOver] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const [guessed, setGuessed] = useState(false);
-  const [currentOriginal, setCurrentOriginal] = useState(originals[0]);
-  const [currentSample, setCurrentSample] = useState(samples[0]);
-  
+  const [gameState, setGameState] = useState(0)
+  const [round, setRound] = useState(0)
+  const [currentRoundScore, setCurrentRoundScore] = useState(0)
 
-  const handleSubmit = (e) => {
-    // e.preventDefault();
-    // const form = e.target;
-    // const formData = new FormData(form);
-    // const formJson = Object.fromEntries(formData.entries());
+  const handleGameStateChange = (newState) => {
+    setGameState(newState)
+  }
 
-    // if (currentSample.title.includes(formJson.guess)) {
-    //   setGuessed(true);
-    // } else {
-    //   console.log(formJson.guess);
-    // }
-    console.log(e);
-    setGuessed(true);
-  };
+  const handleRoundChange = () => {
+    const newRound = round + 1
+    if (newRound >= 5) {
+      console.log("game end")
+    } else {
+      setRound(newRound)
+    }  
+  }
 
-  const handleNext = () => {
-    console.log(counter);
-    console.log(originals.length);
-    if (counter + 1 > originals.length - 1) setGameOver(true);
-    else {
-      setCurrentSample(samples[counter + 1]);
-      setCurrentOriginal(originals[counter + 1]);
-      setCounter(counter + 1);
-      setGuessed(false);
-    }
-  };
+  const handleGiveUp = () => {
+    setCurrentRoundScore(0)
+    setRound(round+1)
+  }
+
+  useEffect(() => {
+    
+  }, [round])
 
   return (
     <div className="main">
-      <h1 className="header">sampdle</h1>
-      {gameOver && <img src={winner}></img>}
-      {gameStart === true && gameOver === false ? (
-        <div className="card">
-          {guessed === true ? (
-            <>
-              <YoutubeEmbed
-                embedId={currentSample.youtubeId}
-                startTime={currentSample.startTime}
-              />
-              <button className="guessNextButton" onClick={handleNext}>Next</button>
-            </>
-          ) : (
-            <>
-              <YoutubeEmbed
-                embedId={currentOriginal.youtubeId}
-                startTime={currentOriginal.startTime}
-              />
-              <AutoComplete onSubmit={(guess) => handleSubmit(guess)} />
-            </>
-          )}
-        </div>
-      ) : (
-        <div className="card">
-          <button autoFocus={true} onClick={() => setGameStart(true)}>Start</button>
-        </div>
-      )}
+      <h1>{gameState}</h1>
+      <h1>sampdle</h1>
+      <h3>{new Date().toLocaleDateString('fi-FI')}</h3>
+      {gameState == 0 &&
+      <>
+        <button onClick={() => handleGameStateChange(1)}>Go</button>
+      </>
+      }
+      {gameState == 1 &&
+        <>
+          <button onClick={() => handleGiveUp()}>give up</button>
+          <h2></h2>
+          <button onClick={() => handleGameStateChange(2)}>stop game</button>
+        </>
+      }
+      {gameState == 2 &&
+      <>
+        <h1>Youre winner</h1>
+      </>
+      }
     </div>
   );
 };
